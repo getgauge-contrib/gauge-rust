56c6760dbd4e3ce21ffc5eb4bdc310e35341d162
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-01-17 17:06:36 +0530

    Add version reading from rust.json and help text
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,43 @@
+extern crate rustc_serialize;
+
+use rustc_serialize::json::Json;
+use std::error::Error;
+use std::path::Path;
+use std::fs::File;
+use std::io::Read;
+
 pub mod install;
 pub mod runner;
+
+pub fn plugin_json() -> Json {
+    let file_path = Path::new("rust.json");
+    let mut file = match File::open(&file_path) {
+        Err(why) => {
+            panic!("Couldn't open plugin meta file {}: {}",
+                   file_path.display(),
+                   Error::description(&why))
+        }
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => {
+            panic!("Couldn't read plugin meta file {}: {}",
+                   file_path.display(),
+                   Error::description(&why))
+        }
+        Ok(content) => content,
+    };
+
+    match Json::from_str(&s) {
+        Err(why) => panic!("Couldn't parse plugin JSON: {}", Error::description(&why)),
+        Ok(jsoncontent) => jsoncontent,
+    }
+}
+
+pub fn version() -> String {
+    let json = plugin_json();
+    let ver = json.find_path(&["version"]).unwrap();
+    ver.to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,22 @@ extern crate gauge_rs;
 
 use std::env;
 
-fn help () {
-    println!("Help");
+fn help() {
+    println!("Rust language runner for Gauge. Version: {}",
+             gauge_rs::version());
+    println!("\nAvailable commands:");
+    println!(" --init - Create a new Gauge project with Rust language runner in the current \
+              directory");
+    println!(" --run  - Run the plugin. Needs to be invoked from Gauge");
+    println!(" --help - Show this help message");
 }
 
-fn main () {
+fn main() {
     if let Some(arg) = env::args().nth(1) {
         match arg.as_ref() {
             "--run" => gauge_rs::runner::run(),
             "--init" => gauge_rs::install::init(),
-            _ => help()
+            _ => help(),
         }
     } else {
         help()
