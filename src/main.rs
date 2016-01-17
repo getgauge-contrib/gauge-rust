cc3e75590e911831ee1a1d6d083c5be1de4ff094
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-01-17 09:46:57 +0530

    Create basic code structure with commandline parsing
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate gauge_rs;
+
+use std::env;
+
+fn help () {
+    println!("Help");
+}
+
 fn main () {
-    println!("gauge-rs: Rust language runner for Gauge");
+    if let Some(arg) = env::args().nth(1) {
+        match arg.as_ref() {
+            "--run" => gauge_rs::runner::run(),
+            "--init" => gauge_rs::install::init(),
+            _ => help()
+        }
+    } else {
+        help()
+    }
 }
