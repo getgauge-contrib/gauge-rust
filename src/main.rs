be509f499b0281b32ba9bc8ab1b97766f29ee5a9
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-01-18 00:58:11 +0530

    Add support to print --version
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,15 @@ fn help() {
     println!("Rust language runner for Gauge. Version: {}",
              gauge_rs::version());
     println!("\nAvailable commands:");
-    println!(" --init - Create a new Gauge project with Rust language runner in the current \
+    println!(" --init    - Create a new Gauge project with Rust language runner in the current \
               directory");
-    println!(" --run  - Run the plugin. Needs to be invoked from Gauge");
-    println!(" --help - Show this help message");
+    println!(" --run     - Run the plugin. Needs to be invoked from Gauge");
+    println!(" --version - Show plugin version");
+    println!(" --help    - Show this help message");
+}
+
+fn print_version() {
+    println!("{}", gauge_rs::version().trim_matches('"'));
 }
 
 fn main() {
@@ -17,6 +22,7 @@ fn main() {
         match arg.as_ref() {
             "--run" => gauge_rs::runner::run(),
             "--init" => gauge_rs::install::init(),
+            "--version" => print_version(),
             _ => help(),
         }
     } else {
