493417c698813042cc71f90be0a3cebcb3b9405b
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-01-20 13:10:55 +0530

    Use error checking in install
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- src/install.rs
+++ src/install.rs
@@ -5,7 +5,9 @@ pub fn init() {
     let step_implementations_dir = path_to(CONFIG.step_implementations_path, Root::Project);
     let steps_file = CONFIG.steps_file;
 
-    create_dir(&step_implementations_dir);
-    copy_file(&skel_dir.join(&steps_file),
-              &step_implementations_dir.join(&steps_file));
+    create_dir(&step_implementations_dir).unwrap();
+    match copy_file(&skel_dir.join(&steps_file), &step_implementations_dir.join(&steps_file)) {
+        Ok(_) => println!(" created {}", &steps_file),
+        Err(e) => println!("Error: {}", e),
+    }
 }
