9b2169d32ec3de9985ecca7116829b19fd119e3c
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2016-07-15 08:28:55 +0530

    Avoid panicking if no test dir is found
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- src/scanner.rs
+++ src/scanner.rs
@@ -3,7 +3,8 @@ use std::fs::DirEntry;
 use std::ffi::OsStr;
 
 fn parse_test_file(entry: &DirEntry) {
-    println!("Will parse: {:?}", entry.file_name());
+    println!("Parsing: {:?}", entry.file_name());
+    // TODO: Parse file here
 }
 
 fn scan_result(entry: &DirEntry) {
@@ -13,4 +14,9 @@ fn scan_result(entry: &DirEntry) {
     }
 }
 
-pub fn scan() { visit_dirs(&CONFIG.step_implementations_path, &scan_result).unwrap(); }
+pub fn scan() {
+    println!("Scanning: {:?}", &CONFIG.step_implementations_path);
+    if let Err(e) =  visit_dirs(&CONFIG.step_implementations_path, &scan_result) {
+        println!("Error trying to scan implementations: {:?}", e)
+    }
+}
